e6f7b26be5e8b0cbaf6962dddf8924503541719d
Oren Sternheim <orensternheim@pingidentity.com>
2024-06-21 16:48:41 +0300

    Fix dest IP address

--- pinggy_tunnel/run.sh
+++ pinggy_tunnel/run.sh
@@ -6,12 +6,12 @@ TOKEN=$(jq --raw-output '.pinggy_token' $CONFIG_PATH)
 
 if [ -z "$TOKEN" ]; then
   while true; do 
-    ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -p 443 -R 0:localhost:8123 a.pinggy.io
+    ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -p 443 -R 0:172.30.32.1:8123 a.pinggy.io
     sleep 10
   done
 else
   while true; do 
-    ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -p 443 -R 0:localhost:8123 ${TOKEN}@a.pinggy.io
+    ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -p 443 -R 0:172.30.32.1:8123 ${TOKEN}@a.pinggy.io
     sleep 10
   done
 fi

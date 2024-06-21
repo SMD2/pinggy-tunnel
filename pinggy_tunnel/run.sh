cefc9a60121f4bc0a5eb6a5b6e6bd9454db2ca48
Oren Sternheim <orensternheim@pingidentity.com>
2024-06-21 23:24:42 +0300

    Log fix

--- pinggy_tunnel/run.sh
+++ pinggy_tunnel/run.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Path to the log file
-LOG_FILE=/data/pinggy_tunnel.log
-
-# Create the log directory if it doesn't exist
-mkdir -p $(dirname $LOG_FILE)
-
-# Clear the log file
-: > $LOG_FILE
-
 CONFIG_PATH=/data/options.json
 
 TOKEN=$(jq --raw-output '.pinggy_token' $CONFIG_PATH)
@@ -22,12 +13,14 @@ fi
 
 if [ -z "$TOKEN" ]; then
   while true; do 
-    ssh $SSH_OPTIONS a.pinggy.io >> $LOG_FILE 2>&1
+    echo "Starting SSH tunnel without token..." # Log message for visibility
+    ssh $SSH_OPTIONS a.pinggy.io
     sleep 10
   done
 else
   while true; do 
-    ssh $SSH_OPTIONS ${TOKEN}@a.pinggy.io >> $LOG_FILE 2>&1
+    echo "Starting SSH tunnel with token..." # Log message for visibility
+    ssh $SSH_OPTIONS ${TOKEN}@a.pinggy.io
     sleep 10
   done
 fi
